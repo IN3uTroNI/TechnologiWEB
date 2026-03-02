88ce4113932913129162a029f63d1faf57c10786
IN3uTroNI <136639106+IN3uTroNI@users.noreply.github.com>
2026-03-02 14:47:38 +0200

    ceva acolo sa fie

---
+++ lab2/scripts/scripts_index.js
@@ -0,0 +1,11 @@
+document.getElementById("mancare").onclick = function () {
+    location.href = "pages/cafeteria.html";
+}
+
+document.getElementById("citit").onclick = function () {
+    location.href = "pages/library.html";
+}
+
+document.getElementById("chestii").onclick = function () {
+    location.href = "pages/secretariat.html";
+}
